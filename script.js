0f729752a5ed23b77df526f19b7f54e5a90c51a2
jamilhilder <139645194+jamilhilder@users.noreply.github.com>
2023-07-15 12:47:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    document.getElementById("brand-link").addEventListener("click", function(event) {
+        event.preventDefault(); // Evita que se siga el enlace predeterminado
+        window.open("", "_blank"); // Abre una nueva ventana en blanco
+    });
+</script>
