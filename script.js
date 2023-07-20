a9ecfbd4e0435d41bbb85570bab78df5779cb9ab
Vladimiry322 <139919339+Vladimiry322@users.noreply.github.com>
2023-07-20 08:51:12 -0500

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-<script>
-    document.getElementById("brand-link").addEventListener("click", function(event) {
-        event.preventDefault(); // Evita que se siga el enlace predeterminado
-        window.open("", "_blank"); // Abre una nueva ventana en blanco
-    });
-</script>
+// Función para mostrar la ventana modal
+function mostrarModal() {
+    var modal = document.getElementById("miModal");
+    modal.style.display = "block";
+  }
+  
+  // Función para cerrar la ventana modal
+  function cerrarModal() {
+    var modal = document.getElementById("miModal");
+    modal.style.display = "none";
+  }
+  
+  // Función para realizar la compra (aquí puedes agregar la lógica para procesar la información ingresada)
+  function realizarCompra() {
+    var nombre = document.querySelector('#miModal input:nth-of-type(1)').value;
+    var direccion = document.querySelector('#miModal input:nth-of-type(2)').value;
+    var cuenta = document.querySelector('#miModal input:nth-of-type(3)').value;
+    
+    // Aquí puedes hacer lo que necesites con la información ingresada, como enviarla a través de un formulario o procesarla para realizar el pago.
+    
+    // Cerrar la ventana modal después de la compra
+    cerrarModal();
+  }
+  
